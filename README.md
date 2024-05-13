99c0e04718205342ddf9641baca1881c9c35b16b
Thiago Heinzen <thiagoheinzen1@gmail.com>
2024-05-13 19:09:48 -0300

    update readme, indentation

--- README.md
+++ README.md
@@ -10,33 +10,43 @@
 </p>
 
 ## Descrição
+
 POMOROBOT é uma aplicação web projetada para ajudar os usuários a manterem o foco em suas tarefas, eliminando distrações e mantendo o fluxo de trabalho. Esta aplicação utiliza a técnica de Pomodoro, oferecendo períodos de trabalho concentrado seguidos por intervalos de descanso.
 
 ## Funcionalidades
+
 - Iniciar ciclos de trabalho com cronômetro ajustável.
 - Escolher entre períodos de descanso curtos e longos.
 - Opção de tocar música durante os ciclos de trabalho.
 - Adicionar, remover e limpar tarefas da lista.
 
 ## Como Usar
-1. Clone este repositório.
+
+1. Clone este repositório ou Clique [aqui](https://github.com/thiagoheinzen/pomorobot/archive/refs/heads/main.zip) para baixar o projeto completo em formato ZIP.
 2. Abra o arquivo `index.html` em seu navegador web.
 3. Comece a usar o POMOROBOT para gerenciar suas tarefas com eficiência!
 
 ## Tecnologias Utilizadas
+
 - HTML
 - CSS
 - JavaScript
 
 ## Autor
+
 Este projeto foi desenvolvido por Thiago Heinzen.
 
 ## Agradecimentos
+
 - Instrutores da Alura Cursos online de Tecnologia, onde em diversos cursos pude aprender a desenvolver este projeto.
 - Imagens geradas por IA no CGDream.
-- Reset CSS por [meyer-reset](https://cdnjs.com/libraries/meyer-reset). 
+- Reset CSS por [meyer-reset](https://cdnjs.com/libraries/meyer-reset).
+- Efeitos sonoros e músicas:<br />
+  Beep - Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=95068">Pixabay</a><br />
+  Pause - Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=61522">Pixabay</a><br />
+  Play - Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6104">Pixabay</a><br />
+  Music by <a href="https://pixabay.com/users/white_records-32584949/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=200223">Maksym Dudchyk</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=200223">Pixabay</a>
 
 ## Notas
+
 Este é um projeto fictício e sem fins comerciais.
-
-
--- script-crud.js
+++ script-crud.js
@@ -1,32 +1,34 @@
 // Selecting DOM elements
-const btnAddTask = document.querySelector('.app__button--add-task');
-const addTaskForm = document.querySelector('.app__form-add-task');
-const textarea = document.querySelector('.app__form-textarea');
-const tasksList = document.querySelector('.app__section-task-list');
-const btnCancel = document.querySelector('.app__form-footer__button--cancel');
-const taskDescriptionParagraph = document.querySelector('.app__section-active-task-description');
+const btnAddTask = document.querySelector(".app__button--add-task");
+const addTaskForm = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+const tasksList = document.querySelector(".app__section-task-list");
+const btnCancel = document.querySelector(".app__form-footer__button--cancel");
+const taskDescriptionParagraph = document.querySelector(
+  ".app__section-active-task-description"
+);
 
 // Selecting buttons for removing completed tasks and all tasks
-const btnRemoveCompleted = document.querySelector('#btn-remove-completed');
-const btnRemoveAll = document.querySelector('#btn-remove-all');
+const btnRemoveCompleted = document.querySelector("#btn-remove-completed");
+const btnRemoveAll = document.querySelector("#btn-remove-all");
 
 // Initializing tasks from localStorage or an empty array
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 let selectedTask = null;
 let selectedTaskListItem = null;
 
 // Function to update tasks in localStorage
-function updateTasks () {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+function updateTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Function to create task elements dynamically
 function createTaskElement(task) {
-    const li = document.createElement('li');
-    li.classList.add('app__section-task-list-item');
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
 
-    const svg = document.createElement('svg');
-    svg.innerHTML = `
+  const svg = document.createElement("svg");
+  svg.innerHTML = `
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
             xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
@@ -35,117 +37,119 @@ function createTaskElement(task) {
         </svg>
     `;
 
-    const paragraph = document.createElement('p');
-    paragraph.textContent = task.description;
-    paragraph.classList.add('app__section-task-list-item-description');
+  const paragraph = document.createElement("p");
+  paragraph.textContent = task.description;
+  paragraph.classList.add("app__section-task-list-item-description");
 
-    const button = document.createElement('button');
-    button.classList.add('app__button-edit');
+  const button = document.createElement("button");
+  button.classList.add("app__button-edit");
 
- // Click event to edit task description
-    button.onclick = () => {
-        const newDescription = prompt("Qual é o novo nome da tarefa?");
-        if (newDescription) {            
-            paragraph.textContent = newDescription;
-            task.description = newDescription;
-            updateTasks();
-        }
-    };
-
-    const buttonImage = document.createElement('img');
-    buttonImage.setAttribute('src', '/images/edit.png');
-    button.append(buttonImage);
-
-    li.append(svg);
-    li.append(paragraph);
-    li.append(button);
-
-// Check if task is completed and set appropriate styling
-    if (task.completed) {
-        li.classList.add('app__section-task-list-item-complete');
-        button.setAttribute('disabled', 'disabled');
-    } else {
-        // Click event to select task and show description
-        li.onclick = () => {
-            document.querySelectorAll('.app__section-task-list-item-active')
-                .forEach(element => {
-                    element.classList.remove('app__section-task-list-item-active');
-                });
-            if (selectedTask == task) {
-                taskDescriptionParagraph.textContent = '';
-                selectedTask = null;
-                selectedTaskListItem = null;
-                return;
-            }
-            selectedTask = task;
-            selectedTaskListItem = li;
-            taskDescriptionParagraph.textContent = task.description;
-           
-            li.classList.add('app__section-task-list-item-active');
-        };
+  // Click event to edit task description
+  button.onclick = () => {
+    const newDescription = prompt("Qual é o novo nome da tarefa?");
+    if (newDescription) {
+      paragraph.textContent = newDescription;
+      task.description = newDescription;
+      updateTasks();
     }
-   
-    return li;
-}   
+  };
+
+  const buttonImage = document.createElement("img");
+  buttonImage.setAttribute("src", "/images/edit.png");
+  button.append(buttonImage);
+
+  li.append(svg);
+  li.append(paragraph);
+  li.append(button);
+
+  // Check if task is completed and set appropriate styling
+  if (task.completed) {
+    li.classList.add("app__section-task-list-item-complete");
+    button.setAttribute("disabled", "disabled");
+  } else {
+    // Click event to select task and show description
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((element) => {
+          element.classList.remove("app__section-task-list-item-active");
+        });
+      if (selectedTask == task) {
+        taskDescriptionParagraph.textContent = "";
+        selectedTask = null;
+        selectedTaskListItem = null;
+        return;
+      }
+      selectedTask = task;
+      selectedTaskListItem = li;
+      taskDescriptionParagraph.textContent = task.description;
+
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
+
+  return li;
+}
 
 // Click event to toggle task form visibility
-btnAddTask.addEventListener('click', () => {
-    addTaskForm.classList.toggle('hidden');
+btnAddTask.addEventListener("click", () => {
+  addTaskForm.classList.toggle("hidden");
 });
 
 // Form submission event to add new task
-addTaskForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const task = {
-        description: textarea.value
-    }
-    tasks.push(task);
-    const taskElement = createTaskElement(task);
-    tasksList.append(taskElement);
-    updateTasks();
-    textarea.value = '';
-    addTaskForm.classList.add('hidden');
-})
+addTaskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = {
+    description: textarea.value,
+  };
+  tasks.push(task);
+  const taskElement = createTaskElement(task);
+  tasksList.append(taskElement);
+  updateTasks();
+  textarea.value = "";
+  addTaskForm.classList.add("hidden");
+});
 
 // Loop through tasks and append to the tasks list
-tasks.forEach(task => {
-    const taskElement = createTaskElement(task);
-    tasksList.append(taskElement);
+tasks.forEach((task) => {
+  const taskElement = createTaskElement(task);
+  tasksList.append(taskElement);
 });
 
 // Function to clear the task form
 const clearForm = () => {
-    textarea.value = '';
-    addTaskForm.classList.add('hidden');
+  textarea.value = "";
+  addTaskForm.classList.add("hidden");
 };
 
 // Click event to clear the task form
-btnCancel.addEventListener('click', clearForm);
+btnCancel.addEventListener("click", clearForm);
 
 // Event listener for when focus on a task finishes
-document.addEventListener('FocusFinished', () => {
-    if (selectedTask && selectedTaskListItem) {
-        selectedTaskListItem.classList.remove('app__section-task-list-item-active');
-        selectedTaskListItem.classList.add('app__section-task-list-item-complete');
-        selectedTaskListItem.querySelector('button').setAttribute('disabled', 'disabled');
-        selectedTask.completed = true;
-        updateTasks();
-    }
+document.addEventListener("FocusFinished", () => {
+  if (selectedTask && selectedTaskListItem) {
+    selectedTaskListItem.classList.remove("app__section-task-list-item-active");
+    selectedTaskListItem.classList.add("app__section-task-list-item-complete");
+    selectedTaskListItem
+      .querySelector("button")
+      .setAttribute("disabled", "disabled");
+    selectedTask.completed = true;
+    updateTasks();
+  }
 });
 
 // Function to remove tasks (completed or all)
 const removeTasks = (onlyCompleted) => {
+  let selector = ".app__section-task-list-item";
+  if (onlyCompleted) {
+    selector = ".app__section-task-list-item-complete";
+  }
 
-    let selector = ".app__section-task-list-item";
-    if (onlyCompleted) {
-        selector = ".app__section-task-list-item-complete";
-    }
-
-    document.querySelectorAll(selector).forEach(element => {
-        element.remove();
-    });
-    tasks = onlyCompleted ? tasks.filter(task => !task.completed) : [];
-    updateTasks();
+  document.querySelectorAll(selector).forEach((element) => {
+    element.remove();
+  });
+  tasks = onlyCompleted ? tasks.filter((task) => !task.completed) : [];
+  updateTasks();
 };
 
 // Click event to remove completed tasks
@@ -153,4 +157,3 @@ btnRemoveCompleted.onclick = () => removeTasks(true);
 
 // Click event to remove all tasks
 btnRemoveAll.onclick = () => removeTasks(false);
-
